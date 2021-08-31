89368084771979c3279f2cba149fdb08bfdedd37
sielus <kubasie1997@gmail.com>
2021-08-31 23:44:39 +0200

    pausa menu

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour{
     public static bool gamePause = false;
@@ -25,5 +26,13 @@ public class PauseMenu : MonoBehaviour{
         this.pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         gamePause = true;
+    }
+
+    public void restartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        this.resumeGame();
+    }
+    public void exitGame() {
+        Application.Quit();
     }
 }
